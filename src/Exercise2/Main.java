08e7e0d14861b7e7f40490486a51a555d544fc5d
dedalus <tim.tagil@mail.ru>
2024-08-15 15:02:50 +0300

    Completed Exercise 2: Dive Deep into Sets

--- src/Exercise2/Main.java
+++ src/Exercise2/Main.java
@@ -1,7 +1,18 @@
 package Exercise2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-
+        Set<String> set = new HashSet<>();
+        set.add("Harry Potter");
+        set.add("Hobbit");
+        set.add("Archmage");
+        set.add("Harry Potter");
+        set.add("Hobbit");
+        set.add("The Wonderful Wizard of Oz");
+        set.add("Hobbit");
+        System.out.println(set);
     }
 }

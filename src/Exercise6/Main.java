be813546e45e3cccfc988605eb7c757e625d0f4f
dedalus <tim.tagil@mail.ru>
2024-08-15 17:13:36 +0300

    Completed Exercise 6: Collections Utility Functions

--- src/Exercise6/Main.java
+++ src/Exercise6/Main.java
@@ -1,7 +1,19 @@
 package Exercise6;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
-
+        ArrayList<Integer> array = new ArrayList<>();
+        int size = (int)(Math.random() * 5 + 5);
+        for (int i = 0; i < size; ++i) {
+            array.add((int)(Math.random() * 99));
+        }
+        System.out.println(array);
+        Collections.sort(array, Collections.reverseOrder());
+        System.out.println(array);
+        Collections.shuffle(array);
+        System.out.println(array);
     }
 }

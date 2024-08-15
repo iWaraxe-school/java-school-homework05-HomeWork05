3a2dc5f0fb5031ecc5ee10ffcf94152f659daaf3
dedalus <tim.tagil@mail.ru>
2024-08-15 15:40:46 +0300

    Completed Exercise 4: Queues and Dequeues

--- src/Exercise4/Main.java
+++ src/Exercise4/Main.java
@@ -1,7 +1,22 @@
 package Exercise4;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
-
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < 5; ++i) {
+            q.add((int)(Math.random() * 100));
+        }
+        System.out.println(q);
+        for (int i = 0; i < 2; ++i) {
+            q.poll();
+        }
+        System.out.println(q);
+        for (int i = 0; i < 3; ++i) {
+            q.add((int)(Math.random() * 100));
+        }
+        System.out.println(q);
     }
 }

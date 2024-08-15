3ef82ec619d6942843896c372bf15f31ef85021c
dedalus <tim.tagil@mail.ru>
2024-08-15 15:32:37 +0300

    Completed Exercise 3: Map Out Your Learning

--- src/Exercise3/Main.java
+++ src/Exercise3/Main.java
@@ -1,7 +1,21 @@
 package Exercise3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Timur", 100);
+        map.put("Kirill", 44);
+        map.put("Artem", 91);
+        map.put("Artur", 23);
+        map.put("Katya", 95);
+        map.put("Oleg", 67);
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 90) {
+                System.out.println(entry.getKey());
+            }
+        }
     }
 }

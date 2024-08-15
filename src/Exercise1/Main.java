af89058dfacc60402e007203d94d7d34f6b6017c
dedalus <tim.tagil@mail.ru>
2024-08-15 14:54:31 +0300

    Completed Exercise 1: Introduction to Lists

--- src/Exercise1/Main.java
+++ src/Exercise1/Main.java
@@ -1,7 +1,16 @@
 package Exercise1;
 
+import java.io.PrintStream;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> array = new ArrayList<>();
+        for (int i = 0; i < 10; ++i) {
+            array.add((int)(Math.random() * 100));
+        }
+        for (int i = 0; i < 10; i += 2) {
+            System.out.println(array.get(i));
+        }
     }
 }

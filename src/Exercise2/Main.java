795117d06d506364f97fb30288d21541bd836cd9
acetvertacova <acetvert@gmail.com>
2024-11-04 15:19:12 +0200

    First commit

--- src/Exercise2/Main.java
+++ src/Exercise2/Main.java
@@ -1,7 +1,17 @@
 package Exercise2;
 
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
+        HashSet<String> books = new HashSet<>();
+        books.add("Harry Potter");
+        books.add("1917");
+        books.add("The Shining");
 
+        //Creating a duplicate title to check the unique HashSet's property
+        books.add("Harry Potter");
+
+        System.out.println(books);
     }
 }
--- src/Exercise4/Main.java
+++ src/Exercise4/Main.java
@@ -1,7 +1,28 @@
 package Exercise4;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+        System.out.println(queue);
+
+        //FIFO
+        queue.poll();
+        queue.poll();
+        System.out.println(queue);
+
+
+        queue.add(6);
+        queue.add(7);
+        System.out.println(queue);
 
     }
 }
